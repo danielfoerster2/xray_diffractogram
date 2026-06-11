df0521e90f1e278edfdadcc1bfb9876722e62585
Daniel Forster <67327007+danielfoerster2@users.noreply.github.com>
2026-06-11 15:39:07 +0200

    Correct units xraydb expects A not nm

--- write_f.py
+++ write_f.py
@@ -14,7 +14,7 @@ q_vals = np.loadtxt("q_vals.dat")
 
 with open("f_vals.dat", "w") as f:
     for q in q_vals:
-        print(*[xraydb.f0(e, q/(4*np.pi))[0] for e in el], file=f)
+        print(*[xraydb.f0(e, q/(10*4*np.pi))[0] for e in el], file=f)
 
     for e in el:
         print(xraydb.f1_chantler(e, energy), xraydb.f2_chantler(e, energy), file=f)
